d7da8d403de54467a1287e3d9b9a15b625bdff49
Gustav Dahlman <dahlman76@gmail.com>
2011-12-13 12:21:42 +0100

    first commit

---
+++ BowlingGame.Tests/Game.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGame.Tests
+{
+    class Game
+    {
+        
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        internal void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        internal int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + StrikeBonus(frameIndex);
+                    frameIndex += 1;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 +SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+            return score;
+        }
+
+        private int SumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+    }
+}
---
+++ BowlingGame.Tests/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGame.Tests
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            game = new Game();
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        private void RollStrike()
+        {
+            game.Roll(10);
+        }
+
+        [TestMethod]
+        public void testGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, game.Score());
+        }
+
+        [TestMethod]
+        public void TestOnStrike()
+        {
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(16, 0);
+            Assert.AreEqual(24, game.Score());
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.AreEqual(300, game.Score());
+        }
+
+
+    }
+}
